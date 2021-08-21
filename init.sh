a4ae427a704c4755e382b5fce6766e22bac59cac
Christina O'Donnell <christina.odonnell@protonmail.com>
2021-08-21 11:39:53 +0100

    Add init.sh

---
+++ init.sh
@@ -0,0 +1,14 @@
+export PATH="/home/cdo/scpt:$PATH"
+
+export EDITOR=edit
+export TERMINAL=st
+export BROWSER=firefox
+
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
+
+export HISTFILE="$HOME/.local/share/shell/histfile"
+
+export LESSHISTFILE=.local/share/less/hist
+export XAUTHORITY=$HOME/.local/share/x11/xAuthority
